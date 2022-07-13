ba0c5c7865a5c579d01a2efa86fd41580a12b157
Jesse Mitchell <jesse@jmitchellmusic.com>
2022-07-13 16:21:39 -0700

    commit

--- React/APIs/pokemon-api/src/App.js
+++ React/APIs/pokemon-api/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {  Button } from 'react-bootstrap'
+
 function App() {
+
+  const [pokemon, setPokemon] = useState({});
+  const [sendRequest, setSendRequest] = useState(false);
+
+  useEffect(() => {
+    if (sendRequest) {
+      fetch('https://pokeapi.co/api/v2/pokemon/?limit=807')
+        .then(response => response.json())
+        .then(response => {
+          setPokemon(response.results)
+
+        })
+        .catch()
+      setSendRequest(false);
+    }
+  }, [sendRequest]);
+
+  const onClick = (e) => {
+    setSendRequest(true)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="">
+        <Button className="btn btn-success mt-3 mb-3 shadow" onClick={onClick}> Fetch Pokemon</Button>
+        <h2 className="mb-2">List of Pokemon: </h2>
+        <div>
+          <ul className="list-group">
+            {pokemon.length > 0 && pokemon.map((pokemon, index) =>
+              <li className="list-group-item" key={index}>{pokemon.name}</li>
+            )}
+          </ul> 
+        </div>
+      </div>
     </div>
   );
 }
