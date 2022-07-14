c66dc0dcc9a1bb5ca5ae43b9cc79e7d9422e04bf
Jesse Mitchell <jesse@jmitchellmusic.com>
2022-07-14 13:49:47 -0700

    commit

--- React/APIs/routing-practice/src/App.js
+++ React/APIs/routing-practice/src/App.js
@@ -1,26 +1,27 @@
-import React from "react";
-import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import DisplayText from "./components/display-text";
-import DisplayColorWord from "./components/display-color-word";
-import "./App.css";
+/** @format */
+
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import DisplayText from './components/display-text';
+import DisplayColorWord from './components/display-color-word';
+import './App.css';
 
 function App() {
-  
-  return (
-    <BrowserRouter>
-    <div className="App">
-      
-        <Routes>
-          <Route path='/home' element={<Home />} />
-          <Route path='/:text' element={<DisplayText />} />
-          <Route path='/:word/:wordColor/:backgroundColor' element={<DisplayColorWord />} />
-        </Routes>
-        
-      
-    </div>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<div className='App'>
+				<Routes>
+					<Route path='/home' element={<Home />} />
+					<Route path='/:text' element={<DisplayText />} />
+					<Route
+						path='/:word/:wordColor/:backgroundColor'
+						element={<DisplayColorWord />}
+					/>
+				</Routes>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
