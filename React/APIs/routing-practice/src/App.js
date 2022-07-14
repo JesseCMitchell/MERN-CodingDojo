9c0df7ce70fa18c0c6561e356ad079641918b59e
Jesse Mitchell <jesse@jmitchellmusic.com>
2022-07-14 14:25:14 -0700

    commit

--- React/APIs/routing-practice/src/App.js
+++ React/APIs/routing-practice/src/App.js
@@ -1,18 +1,29 @@
 /** @format */
 
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link, useParams } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import './App.css';
+
+const Location = props => {
+	const { city } = useParams(); //matches our :city in our Routes
+	return <h1>Welcome to {city} !</h1>;
+};
 
 function App() {
 	return (
 		<div className='App'>
 			<h1>Routing Test</h1>
+			<p>
+				<Link to='/location/seattle'>Seattle</Link>|
+				<Link to='/location/chicago'>Chicago</Link>|
+				<Link to='/location/burbank'>Burbank</Link>
+			</p>
 			<Routes>
 				<Route path='/' element={<Home />} />
 				<Route path='/about' element={<About />} />
+				<Route path='/location/:city' element={<Location />} />
 			</Routes>
 		</div>
 	);
