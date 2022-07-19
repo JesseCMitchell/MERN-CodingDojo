727568af7acc8dee57eff6b7d94d660cc66e501c
Jesse Mitchell <jesse@jmitchellmusic.com>
2022-07-18 20:20:53 -0700

    commit

--- MongoDB/Mongoose/jokes/models/jokes.model.js
+++ MongoDB/Mongoose/jokes/models/jokes.model.js
@@ -2,10 +2,21 @@
 
 const mongoose = require('mongoose');
 
-const JokesSchema = new mongoose.Schema({
-	setup: String,
-	punchline: String,
-});
+const JokesSchema = new mongoose.Schema(
+	{
+		setup: {
+			type: String,
+			required: [true, 'Setup required'],
+			minlength: [10, 'Setup must be at least 10 characters long'],
+		},
+		punchline: {
+			type: String,
+			required: [true, 'Punchline required'],
+			minlength: [10, 'Punchline must be at least 10 characters long'],
+		},
+	},
+	{ timestamps: true },
+);
 
 const Joke = mongoose.model('joke', JokesSchema);
 
--- MongoDB/Mongoose/jokes/routes/jokes.routes.js
+++ MongoDB/Mongoose/jokes/routes/jokes.routes.js
@@ -6,7 +6,7 @@ module.exports = app => {
 	app.get('/api/jokes/', JokeController.findAllJokes);
 	app.get('/api/jokes/:_id', JokeController.findOneJoke);
 	app.get('/api/jokes/random', JokeController.randomJoke);
-	app.get('/api/jokes/new', JokeController.createNewJoke);
-	app.get('/api/jokes/update/:_id', JokeController.updateJoke);
-	app.get('/api/jokes/delete/:_id', JokeController.deleteJoke);
+	app.post('/api/jokes/new', JokeController.createNewJoke);
+	app.put('/api/jokes/update/:_id', JokeController.updateJoke);
+	app.delete('/api/jokes/delete/:_id', JokeController.deleteJoke);
 };
